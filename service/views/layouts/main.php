ccea3df4be20f35026059292655abc82f620d07b
xinxiangmo <xinxiangmo@gmail.com>
2012-08-24 09:32:01 +0800

    网站域名放到根目录下

--- service/views/layouts/main.php
+++ service/views/layouts/main.php
@@ -2,20 +2,20 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<script src="<?php echo Yii::app()->baseUrl; ?>/static/tlabs-min.js" async=""></script>
-<script src="<?php echo Yii::app()->baseUrl; ?>/static/tb-mpp-min.js" async=""></script>
+<script src="./static/tlabs-min.js" async=""></script>
+<script src="./static/tb-mpp-min.js" async=""></script>
 <meta charset="GBK">
 <title>秒杀专区首页 - 汇淘宝精品 - 淘宝网超优汇_每天10:00开始抢购！</title>
-<link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/static/common.css" type="text/css">
+<link rel="stylesheet" href="./static/common.css" type="text/css">
 <!-- start vmc css 3.1-->
-<link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/static/header-min.css" type="text/css">
+<link rel="stylesheet" href="./static/header-min.css" type="text/css">
 <!-- end vmc css 3.1-->
-<link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/static/index_v2.css" type="text/css">
-<link rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/static/index.css" type="text/css">
+<link rel="stylesheet" href="./static/index_v2.css" type="text/css">
+<link rel="stylesheet" href="./static/index.css" type="text/css">
 <script type="text/javascript"
-	src="<?php echo Yii::app()->baseUrl; ?>/static/global-min.js"></script>
+	src="./static/global-min.js"></script>
 <style type="text/css"></style>
-<link id="yui__dyn_0" type="text/css" charset="utf-8" rel="stylesheet" href="<?php echo Yii::app()->baseUrl; ?>/static/tbra.css">
+<link id="yui__dyn_0" type="text/css" charset="utf-8" rel="stylesheet" href="./static/tbra.css">
 <!--原来的head-->
 <!--原来的head end-->
 </head>
