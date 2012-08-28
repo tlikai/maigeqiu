85f0ef7536a97d2c97ef5a49310a4523409ebdec
likai <tlikai@qq.com>
2012-08-28 17:18:38 +0800

    优化代码

--- manager/protected/models/Goods.php
+++ manager/protected/models/Goods.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * This is the model class for table "goods".
- *
- * The followings are the available columns in table 'goods':
+ * 商品
+ * 
  * @property integer $id
  * @property string $title
  * @property string $short_title
@@ -16,73 +15,51 @@
  * @property string $shop_url
  * @property integer $add_time
  * @property string $image_url
+ * @property integer $recommend
  */
 class Goods extends CActiveRecord
 {
 	public $keyword = null;
 	public $cid = null;
 	public $tbid = null;
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Goods the static model class
-	 */
+	
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
 	}
 
-	/**
-	 * @return string the associated database table name
-	 */
 	public function tableName()
 	{
-		return 'goods';
+		return '{{goods}}';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
 			array('title, url, shop_url', 'required'),
-			array('cat_id, quantity, start_time, end_time,recommend, add_time', 'numerical', 'integerOnly'=>true),
+			array('cat_id, quantity, start_time, end_time, recommend, add_time', 'numerical', 'integerOnly' => true),
 			array('price, sale_price', 'numerical'),
-			array('title, url, shop_url, image_url,shop_name', 'length', 'max'=>255),
-			array('short_title,tb_id', 'length', 'max'=>100),
-			// array('tb_id','checkTbId'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, title, tb_id,shop_name,recommend,commission,short_title, price, sale_price, quantity, start_time, end_time, url, shop_url, add_time, image_url', 'safe', 'on'=>'search'),
+			array('title, url, shop_url, image_url, shop_name', 'length', 'max' => 255),
+			array('short_title,tb_id', 'length', 'max' => 100),
+		        
+			array('id, title, tb_id, shop_name, recommend, commission, short_title, price, sale_price, quantity, start_time, end_time, url, shop_url, add_time, image_url', 'safe', 'on'=>'search'),
 		);
 	}
 
 	public static function checkTbId($tb_id)
 	{
 		$resetGoods = Goods::model()->find(array(
-				'condition'=>'tb_id=:tb_id',
-				'params'=>array(':tb_id'=>$tb_id)
+            'condition'=>'tb_id=:tb_id',
+            'params'=>array(':tb_id'=>$tb_id)
 		));
 		return $resetGoods;
 	}
 	
-	/**
-	 * @return array relational rules.
-	 */
 	public function relations()
 	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
+		return array();
 	}
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
 	public function attributeLabels()
 	{
 		return array(
@@ -108,31 +85,24 @@ class Goods extends CActiveRecord
 		);
 	}
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
 	public function search()
 	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('short_title',$this->short_title,true);
-		$criteria->compare('price',$this->price);
-		$criteria->compare('sale_price',$this->sale_price);
-		$criteria->compare('quantity',$this->quantity);
-		$criteria->compare('start_time',$this->start_time);
-		$criteria->compare('end_time',$this->end_time);
-		$criteria->compare('url',$this->url,true);
-		$criteria->compare('shop_url',$this->shop_url,true);
-		$criteria->compare('add_time',$this->add_time);
-		$criteria->compare('image_url',$this->image_url,true);
-		$criteria->compare('tb_id',$this->tb_id,true);
-		$criteria->compare('recommend',$this->recommend,true);
+        $criteria = new CDbCriteria();
+        
+        $criteria->compare('id', $this->id);
+        $criteria->compare('title', $this->title, true);
+        $criteria->compare('short_title', $this->short_title, true);
+        $criteria->compare('price', $this->price);
+        $criteria->compare('sale_price', $this->sale_price);
+        $criteria->compare('quantity', $this->quantity);
+        $criteria->compare('start_time', $this->start_time);
+        $criteria->compare('end_time', $this->end_time);
+        $criteria->compare('url', $this->url, true);
+        $criteria->compare('shop_url', $this->shop_url, true);
+        $criteria->compare('add_time', $this->add_time);
+        $criteria->compare('image_url', $this->image_url, true);
+        $criteria->compare('tb_id', $this->tb_id, true);
+        $criteria->compare('recommend', $this->recommend, true);
 		$criteria->order = 't.id Desc';
 
 		return new CActiveDataProvider($this, array(
@@ -140,128 +110,123 @@ class Goods extends CActiveRecord
 		));
 	}
 
-	public static function getTaobaoKeLink($num_iids) {
-		  
+    public static function getTaobaoKeLink($num_iids)
+    {
 		$click_urls = array();
-		  foreach ($num_iids AS $num_iid) {
-			if (!isset($click_urls[$num_iid])) {
-			  $tbapi_num_iids_arr[] = $num_iid;
-			}
-		  }
-		  
-		  if (!empty($tbapi_num_iids_arr)) {
-			$numbers = count($tbapi_num_iids_arr);
-			$numbers_max = 40; //淘宝 API 限制最大返回40条记录
-			if ($numbers > 0) {
-			  $numbers_times = ceil($numbers / $numbers_max); //第一层循环的循环次数
-			  $numbers_start = 0;
-			  $numbers_end = $numbers_max;
-			  for ($numbers_i = 1; $numbers_i <= $numbers_times; $numbers_i++) {
-				for ($numbers_j = $numbers_start; $numbers_j < $numbers_end; $numbers_j++) {
-				  if ($numbers_j >= $numbers) {
-					break;
-				  }
-				  $tbapi_num_iids_arr_sp[] = $tbapi_num_iids_arr[$numbers_j];
-				}
-				
-				$numbers_start = $numbers_start + $numbers_max;
-				$numbers_end = $numbers_end + $numbers_max;
-				
-				$tbapi_num_iids = implode(",", $tbapi_num_iids_arr_sp);
-				$c = new TopClient;
-				$c->appkey = Yii::app()->params['appkey']; //淘宝开放平台 API 接口 App Key
-				$c->secretKey = Yii::app()->params['secretKey']; //淘宝开放平台 API 接口 App Secret
-				$c->format = "json";
-				$req = new TaobaokeItemsConvertRequest;
-				$req->setFields("num_iid,title,nick,pic_url,price,click_url,commission,commission_rate,commission_num,commission_volume,shop_click_url,seller_credit_score,item_location,volume");
-				$req->setNumIids($tbapi_num_iids);
-				$req->setPid(Yii::app()->params['pid']); //淘宝联盟（阿里妈妈）PID
-				$resp = $c->execute($req);
-				$res = object2Array($resp);
-		  
-				if (isset($res["taobaoke_items"]["taobaoke_item"])) {
-				  $links = $res["taobaoke_items"]["taobaoke_item"];
-				  foreach ($links as $value) {
-					$click_urls[] = $value;
-				  }
-				}
-				
-				unset($tbapi_num_iids_arr_sp);
-				unset($tbapi_num_iids);
-				unset($resp);
-				unset($res);
-				unset($links);
-				unset($value);
-			  }
-			}
-		  }
-		  
-		  return $click_urls;
+        foreach($num_iids as $num_iid)
+        {
+            if(!isset($click_urls[$num_iid]))
+            {
+                $tbapi_num_iids_arr[] = $num_iid;
+            }
+        }
+        
+        if(!empty($tbapi_num_iids_arr))
+        {
+            $numbers = count($tbapi_num_iids_arr);
+            $numbers_max = 40; // 淘宝 API 限制最大返回40条记录
+            if($numbers > 0)
+            {
+                $numbers_times = ceil($numbers / $numbers_max); // 第一层循环的循环次数
+                $numbers_start = 0;
+                $numbers_end = $numbers_max;
+                for($numbers_i = 1; $numbers_i <= $numbers_times; $numbers_i++)
+                {
+                    for($numbers_j = $numbers_start; $numbers_j < $numbers_end; $numbers_j++)
+                    {
+                        if($numbers_j >= $numbers)
+                        {
+                            break;
+                        }
+                        $tbapi_num_iids_arr_sp[] = $tbapi_num_iids_arr[$numbers_j];
+                    }
+                    
+                    $numbers_start = $numbers_start + $numbers_max;
+                    $numbers_end = $numbers_end + $numbers_max;
+                    $tbapi_num_iids = implode(",", $tbapi_num_iids_arr_sp);
+                    
+                    $req = new TaobaokeItemsConvertRequest();
+                    $req->setFields("num_iid,title,nick,pic_url,price,click_url,commission,commission_rate,commission_num,commission_volume,shop_click_url,seller_credit_score,item_location,volume");
+                    $req->setNumIids($tbapi_num_iids);
+                    $req->setPid(Yii::app()->params['pid']); // 淘宝联盟（阿里妈妈）PID
+                    
+                    $resp = Yii::app()->topClient->execute($req);
+                    $res = Yii::app()->topClient->processResponse($resp);
+                    
+                    if(isset($res["taobaoke_items"]["taobaoke_item"]))
+                    {
+                        $links = $res["taobaoke_items"]["taobaoke_item"];
+                        foreach($links as $value)
+                        {
+                            $click_urls[] = $value;
+                        }
+                    }
+                    
+                    unset($tbapi_num_iids_arr_sp);
+                    unset($tbapi_num_iids);
+                    unset($resp);
+                    unset($res);
+                    unset($links);
+                    unset($value);
+                }
+            }
+        }
+        
+        return $click_urls;
 	}
 
-	/**
-	 * 处理商品数据
-	 */
-	public static function processingItems($ids)
-	{
-		$c = new TopClient;
-		$c->appkey = Yii::app()->params['appkey']; //淘宝开放平台 API 接口 App Key
-		$c->secretKey = Yii::app()->params['secretKey']; //淘宝开放平台 API 接口 App Secret
-		$c->format = "json";
-
-		if(count($ids) > 20 ) {
-		
-			$tmp1 = array_splice($ids,0,20);
-			$req = new ItemsListGetRequest;
-			$req->setFields("delist_time,list_time,num_iid,has_showcase");
-			$req->setNumIids(implode(',',$tmp1));
-			$resp = $c->execute($req);
-			$res1 = object2Array($resp);
-
-			$req->setNumIids(implode(',',$ids));
-			$resp = $c->execute($req);
-			$res2 = object2Array($resp);
-
-			return array_merge($res1['items']['item'],$res2['items']['item']);
-		
-		} else {
-			$req = new ItemsListGetRequest;
-			$req->setFields("detail_url,num_iid,title,nick,type,cid,seller_cids,props,input_pids,input_str,desc,pic_url,num,valid_thru,list_time,delist_time,stuff_status,location,price,post_fee,express_fee,ems_fee,has_discount,freight_payer,has_invoice,has_warranty,has_showcase,modified,increment,approve_status,postage_id,product_id,auction_point,property_alias,item_img,prop_img,sku,video,outer_id,is_virtual");
-			$req->setNumIids(implode(',',$ids));
-			$resp = $c->execute($req);
-			$res = object2Array($resp);
-			return $res['items']['item'];
-		}
+    /**
+     * 处理商品数据
+     */
+    public static function processingItems($ids)
+    {
+		if(count($ids) > 20)
+        {
+            $tmp1 = array_splice($ids, 0, 20);
+            
+            $req = new ItemsListGetRequest();
+            $req->setFields("delist_time,list_time,num_iid,has_showcase");
+            $req->setNumIids(implode(',', $tmp1));
+            
+            $resp = Yii::app()->topClient->execute($req);
+            $res1 = Yii::app()->topClient->processResponse($resp);
+            
+            $req->setNumIids(implode(',', $ids));
+            $resp = Yii::app()->topClient->execute($req);
+            $res2 = Yii::app()->topClient->processResponse($resp);
+            
+            return array_merge($res1['items']['item'], $res2['items']['item']);
+        }
+        
+        $req = new ItemsListGetRequest();
+        $req->setFields("detail_url,num_iid,title,nick,type,cid,seller_cids,props,input_pids,input_str,desc,pic_url,num,valid_thru,list_time,delist_time,stuff_status,location,price,post_fee,express_fee,ems_fee,has_discount,freight_payer,has_invoice,has_warranty,has_showcase,modified,increment,approve_status,postage_id,product_id,auction_point,property_alias,item_img,prop_img,sku,video,outer_id,is_virtual");
+        $req->setNumIids(implode(',', $ids));
+        $resp = Yii::app()->topClient->execute($req);
+        $res1 = Yii::app()->topClient->processResponse($resp);
+        
+        return $res['items']['item'];
 	}
 
-	public static function processingPromotion($id)
-	{
-		$c = new TopClient;
-		$c->appkey = Yii::app()->params['appkey']; //淘宝开放平台 API 接口 App Key
-		$c->secretKey = Yii::app()->params['secretKey']; //淘宝开放平台 API 接口 App Secret
-		$c->format = "json";
-
-		$promotion = new UmpPromotionGetRequest;
-		$promotion->setItemId($id);
-		$resp = $c->execute($promotion);
-		$res = object2Array($resp);
-
-		if ( !$res ) {
+    public static function processingPromotion($id)
+    {
+		$promotion = new UmpPromotionGetRequest();
+        $promotion->setItemId($id);
+        $resp = Yii::app()->topClient->execute($promotion);
+        $res = Yii::app()->topClient->processResponse($resp);
+        
+        if(!$res)
+            return false;
+        
+        if(!isset($res['promotions']) || !isset($res['promotions']['promotion_in_item']['promotion_in_item']))
 			return false;
-		}
-
-		
-		if ( !isset( $res['promotions'] ) || !isset( $res['promotions']['promotion_in_item']['promotion_in_item'] ) ) {
-			return false;
-		}
 		
 		$promotionArr = $res['promotions']['promotion_in_item']['promotion_in_item']['0'];
-		return array(
-			'price'=>$promotionArr['item_promo_price'],
-			'start_time'=>$promotionArr['start_time'],
-			'end_time'=>$promotionArr['end_time'],
-		);
 		
+		return array(
+			'price' => $promotionArr['item_promo_price'],
+			'start_time' => $promotionArr['start_time'],
+			'end_time' => $promotionArr['end_time'],
+		);
 	}
 
 	/**
@@ -271,56 +236,107 @@ class Goods extends CActiveRecord
 	{
 		$res = array();
 
-		if ( empty($array['keyword']) && empty($array['tbid']) ) {
+		if(empty($array['keyword']) && empty($array['tbid']))
 			return false;
-		}
-
-		$c = new TopClient;
-		$c->appkey = Yii::app()->params['appkey']; //淘宝开放平台 API 接口 App Key
-		$c->secretKey = Yii::app()->params['secretKey']; //淘宝开放平台 API 接口 App Secret
-		$c->format = "json";
 
 		$req = new TaobaokeItemsGetRequest;
 		$req->setFields("num_iid,title,nick,pic_url,price,click_url,commission,commission_rate,commission_num,commission_volume,shop_click_url,seller_credit_score,item_location,volume");
-		$req->setPid(Yii::app()->params['pid']); //淘宝联盟（阿里妈妈）PID
+		$req->setPid(Yii::app()->params['pid']); // 淘宝联盟（阿里妈妈）PID
 		$req->setPageNo($array['page']);
 		$req->setStartPrice(Yii::app()->params['start_price']);
 		$req->setEndPrice(Yii::app()->params['end_price']);
 		$req->setSort(Yii::app()->params['sort']);
+                
+        if(!empty($array['keyword']))
+        {
+            $req->setKeyword($array['keyword']);
+        }
+        else
+        {
+            $newId = array();
+            $newId = explode(',', $array['tbid']);
+            return self::getTaobaoKeLink($newId);
+        }
 
+		$resp = Yii::app()->topClient->execute($req);
+		$res = Yii::app()->topClient->processResponse($resp);
+                
+        if(!empty($res))
+            return $res['taobaoke_items']['taobaoke_item'];
+	}
+	
 
-		if ( !empty($array['keyword']) ) {
-			$req->setKeyword($array['keyword']);
-		} else {
-			$newId = array();
-			$newId = explode(',',$array['tbid']);
-			return self::getTaobaoKeLink($newId);
-		}
-
-		$resp = $c->execute($req);
-		$res = object2Array($resp);
-
-		if ( !empty($res) ) {
-			return $res['taobaoke_items']['taobaoke_item'];
-		}
+	public static function getRecommentGoodsCacheKey( $number ) {
+	    return 'goods_recomment_'.$number;
+	}
+	
+	/**
+	 * 获取推荐商品
+	 * @param unknown_type $number
+	 */
+	public static function getRecommentGoods( $number = '3' )
+	{
+	    $data = false;
+	    $data = Yii::app()->cache->get( self::getRecommentGoodsCacheKey( $number ) );
+	
+	    if ( $data != false ) {
+	        return $data;
+	    }
+	
+	    $goods = self::model()->findAll( array (
+	            'limit' => '3',
+	            'order' => 'recommend Desc',
+	    ) );
+	
+	    Yii::app()->cache->set( self::getRecommentGoodsCacheKey( $number ) , $goods , 3600 );
+	    return $goods;
 	}
 	
 	
-}
-function object2Array($d)
-{
-	if (is_object($d))
+	public static function getGoodsCacheKey($appId, $catId, $page)
 	{
-		$d = get_object_vars($d);
+	    return 'goods_cache_'.$appId.'_'.$catId.'_'.$page;
+	}
+	
+	public static function getGoodsCountCacheKey($catId, $page)
+	{
+	    return 'goods_cache_count_'.$catId.'_'.$page;
+	}
+	
+	
+	
+	public static function getGoods($appId = 1, $catId = 0, $page = 1)
+	{
+	    $data = array('data'=>null);
+	    $data = Yii::app()->cache->get(self::getGoodsCacheKey($appId, $catId, $page));
+	    if($data == false)
+	    {
+	        $criteria = new CDbCriteria;
+	        $criteria->condition = 't.end_time > '.time();
+	        $criteria ->order = 't.quantity DESC';
+	        $catId && $criteria->compare('cat_id', $catId);
+	        $count = Goods::model()->count($criteria);
+	        $pager = new CPagination($count);
+	        $pager->pageSize = 10;
+	        $pager->applyLimit($criteria);
+	        $data =  Goods::model()->findAll($criteria);
+	
+	        if ($appId != 1) {
+	            shuffle($data);
+	        }
+	
+	        $data = array('data'=>$data,'pager'=>$pager);
+	
+	        Yii::app()->cache->set(self::getGoodsCacheKey($appId, $catId, $page),$data,1800);
+	    }
+	    return $data;
 	}
 
-	if (is_array($d))
-	{
-		return array_map(__FUNCTION__, $d);
-	}
-	else
-	{
-		return $d;
+    public static function handleData($array, $appId)
+    {
+    	if(!$array || $appId == 1)
+        	return false;
+        echo $appId;
+    	return shuffle($array);
 	}
 }
-
--- service/controllers/SiteController.php
+++ service/controllers/SiteController.php
@@ -56,29 +56,21 @@ class SiteController extends Controller
 		)
 			
 	);
-	/**
-	 * Declares class-based actions.
-	 */
+	
 	public function actions()
 	{
 		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
 			'captcha'=>array(
 				'class'=>'CCaptchaAction',
 				'backColor'=>0xFFFFFF,
 			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
+		        
 			'page'=>array(
 				'class'=>'CViewAction',
 			),
 		);
 	}
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionIndex()
 	{
 		$page = Yii::app()->request->getQuery('page', 1);
@@ -87,9 +79,9 @@ class SiteController extends Controller
 
         $view = 'index';
         $layout = $this->layout;
-
+        
         $goods = Goods::getGoods($appId, $catId, $page);
-        $cats = Category::model()->getByAppId( $catId  );
+        $cats = Category::model()->getByAppId($catId);
         $this->layout = $layout;
         $this->render($view, array(
             'cats' => $cats,
@@ -137,129 +129,32 @@ class SiteController extends Controller
 	
 	public function actionClearCache()
 	{
-		$files = Array();
-		$path  = Yii::app()->runtimePath.'/cache/';
-		
-		
-		$files = ListFiles($path);
-		if ( !$files ) {
-			die('拒绝访问');
-		}
-			
-		foreach($files as $key => $val)
-		{
-			unlink($val);
-		}
-	
+		Yii::app()->cache->gc(false);
 	}
 	
 	public function actionTaoBaoApp()
 	{
-		$id = Yii::app()->request->getQuery('id');
-		
 		$this->layout = 'app2';
-		
-		$page = Yii::app()->request->getQuery('page','1');
-		$goods = Goods::getGoods($page);
-
-
-		$this->render('../app/index',array(
- 				'data'=>$goods['data'],
- 				'pager'=>$goods['pager'],
+        
+        $id = Yii::app()->request->getQuery('id');
+        $page = Yii::app()->request->getQuery('page', '1');
+        $goods = Goods::getGoods($page);
+        
+		$this->render('../app/index', array(
+ 			'data' => $goods['data'],
+			'pager' => $goods['pager'],
 		));
 		
 	}
-
-	/**
-	 * This is the action to handle external exceptions.
-	 */
+	
 	public function actionError()
 	{
-	    if($error=Yii::app()->errorHandler->error)
-	    {
-	    	if(Yii::app()->request->isAjaxRequest)
-	    		echo $error['message'];
-	    	else
-	        	$this->render('error', $error);
-	    }
-	}
-
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
-
-	/**
-	 * Displays the login page
-	 */
-	public function actionLogin()
-	{
-		$model=new LoginForm;
-
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
-	}
-
-	/**
-	 * Logs out the current user and redirect to homepage.
-	 */
-	public function actionLogout()
-	{
-		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
+	    if($error = Yii::app()->errorHandler->error)
+        {
+            if(Yii::app()->request->isAjaxRequest)
+                echo $error['message'];
+            else
+                $this->render('error', $error);
+        }
 	}
 }
-
-function ListFiles($dir) {
-
-	if($dh = opendir($dir)) {
-
-		$files = Array();
-		$inner_files = Array();
-
-		while($file = readdir($dh)) {
-			if($file != "." && $file != ".." && $file[0] != '.') {
-				if(is_dir($dir . "/" . $file)) {
-					$inner_files = ListFiles($dir . "/" . $file);
-					if(is_array($inner_files)) $files = array_merge($files, $inner_files);
-				} else {
-					array_push($files, $dir . "/" . $file);
-				}
-			}
-		}
-
-		closedir($dh);
-		return $files;
-	}
-}
-
