98627383ba09ffbd532bb34c32fbf474918abedf
xinxiangmo <xinxiangmo@gmail.com>
2012-09-04 23:42:40 +0800

    修改配置文件

--- service/config/main.php
+++ service/config/main.php
@@ -32,7 +32,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=tbk',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 			'tablePrefix' => '',
 		),
